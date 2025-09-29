f0baa396e9321ecada86266803dc7c816dcd7580
Nathaniel Greisen <34870485+doggomeloon@users.noreply.github.com>
2025-09-29 14:37:43 -0700

    Add arrow icon meta files and update WebGL settings
    Added meta files for DownArrow, LeftArrow, RightArrow, and UpArrow sprites. Updated all LetterIcons sprite meta files to include platform-specific import settings for WebGL, improving compatibility and texture handling for WebGL builds.

--- Lancers Stand/Assets/Scripts/World/GlobalVariables.cs
+++ Lancers Stand/Assets/Scripts/World/GlobalVariables.cs
@@ -19,7 +19,8 @@ public static class GlobalVariables
     public static string[] eligibleKeys = new string[] {
     "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
     "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-    "LeftShift", "Space", "Equals"
+    "LeftShift", "Space", "Equals",
+    "LeftArrow", "RightArrow", "UpArrow", "DownArrow"
     };
     public static KeyCode leftKey = KeyCode.A;
     public static KeyCode rightKey = KeyCode.D;
